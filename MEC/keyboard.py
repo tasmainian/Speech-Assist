9eb508559a43b66552dd13bd9d04e4c683bfd6b4
unknown <christianhallgrimson@yahoo.ca>
2018-10-20 18:33:00 -0400

    action bar

--- MEC/keyboard.py
+++ MEC/keyboard.py
@@ -62,6 +62,10 @@ class KeyboardWidget(GridLayout):
 
     def clear(self, *args):
         self.ids.calc_input.text = ''
+		
+	#used to update the text from the quickselect menu
+	def updateT(self,text):
+		self.ids.calc_input.text += text	#appends the text to the inputbar
 
     def speak(self, *args):
         sentence = self.ids.calc_input.text
